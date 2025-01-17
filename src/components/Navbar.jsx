e775052df50884e3c259f3f213d2a094261b742e
shoyonp <shoyonkumar143@gmail.com>
2025-01-17 12:21:00 +0600

    initial navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/assets/icons8-forum-96.png";
+import { IoNotifications } from "react-icons/io5";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/membership">Membership</NavLink>
+      <NavLink to="/login">Join US</NavLink>
+    </>
+  );
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <img src={logo} className="w-8" alt="" />
+          <a className="btn btn-ghost text-xl">
+            <span>S</span>ocial <span>P</span>od
+          </a>
+        </div>
+        <div className="flex">
+          <div>
+            <ul className="flex gap-3">{links}</ul>
+          </div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <div className="indicator">
+                <IoNotifications className="text-2xl"></IoNotifications>
+                <span className="badge badge-sm indicator-item">8</span>
+              </div>
+            </div>
+          </div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>User name</a>
+              </li>
+              <li>
+                <a>Dashboard</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
