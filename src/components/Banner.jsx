2037a38f3e0674d6bea6f1dc2732b2847f396d74
shoyonp <shoyonkumar143@gmail.com>
2025-05-13 17:04:53 +0600

    major changes in banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,6 +2,7 @@ import Marquee from "react-fast-marquee";
 import useTag from "../hooks/useTag";
 import { useState } from "react";
 import usePost from "../hooks/usePost";
+import banner from "../assets/assets/banner.jpg"
 
 const Banner = () => {
   const [tags, loading] = useTag();
@@ -23,29 +24,36 @@ const Banner = () => {
   // console.log("searching for :", search);
   return (
     <>
-      <div className="bg-gradient-to-r from-blue-400 to-blue-500 p-6 md:p-12 text-white shadow-md  w-full mx-auto ">
-        <h1 className="text-3xl md:text-4xl font-semibold mb-6 text-center">
-          Search Posts
-        </h1>
-        <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-4">
-          <input
-            type="text"
-            placeholder="Search by tags..."
-            className="p-3 w-11/12 md:w-1/2 rounded-md shadow-md focus:outline-none focus:ring-2 text-gray-800 transition transform duration-300 ease-in-out hover:scale-105"
-            onChange={(e) => setInputText(e.target.value)}
-            value={inputText}
-          />
-          <button
-            onClick={handleSearch}
-            className="bg-blue-600 p-3 w-11/12 md:w-auto rounded-md text-white shadow-md hover:bg-blue-700 transition duration-300 ease-in-out transform hover:scale-105"
-          >
-            Search
-          </button>
-          <button className="bg-gradient-to-r from-blue-400 to-blue-600 p-3 w-11/12 md:w-auto rounded-md text-white shadow-md hover:from-blue-500 hover:to-blue-700 transition duration-300 ease-in-out transform hover:scale-105">
-            Sort by Popularity
-          </button>
-        </div>
-      </div>
+      <div
+  className="relative bg-cover h-[300px] bg-center p-6 md:p-12 md:pt-16 text-white shadow-md w-full mx-auto "
+  style={{ backgroundImage: `url(${banner})` }}
+>
+  <div className="absolute inset-0 bg-black bg-opacity-50 z-0"></div>
+  <div className="relative z-10 ">
+    <h1 className="text-3xl md:text-4xl font-semibold mb-6 text-center">
+      Search Posts
+    </h1>
+    <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-4">
+      <input
+        type="text"
+        placeholder="Search by tags..."
+        className="p-3 w-11/12 md:w-1/2 rounded-md shadow-md focus:outline-none focus:ring-2 text-gray-800 transition transform duration-300 ease-in-out hover:scale-105"
+        onChange={(e) => setInputText(e.target.value)}
+        value={inputText}
+      />
+      <button
+        onClick={handleSearch}
+        className="bg-blue-600 p-3 w-11/12 md:w-auto rounded-md text-white shadow-md hover:bg-blue-700 transition duration-300 ease-in-out transform hover:scale-105"
+      >
+        Search
+      </button>
+      <button className="bg-gradient-to-r from-blue-400 to-blue-600 p-3 w-11/12 md:w-auto rounded-md text-white shadow-md hover:from-blue-500 hover:to-blue-700 transition duration-300 ease-in-out transform hover:scale-105">
+        Sort by Popularity
+      </button>
+    </div>
+  </div>
+</div>
+
 
       <div className="flex justify-center space-x-3 mt-8 w-full mx-auto">
         <Marquee pauseOnHover={true}>
